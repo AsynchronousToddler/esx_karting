b6bc5e571744ee3c597ca97c6a04abda05c8962b
Guiyomee <guiyomee29200@gmail.com>
2018-03-17 21:07:53 +0100

    esx_karting

---
+++ __resource.lua
@@ -0,0 +1,17 @@
+description 'esx_karting.lua'
+
+server_scripts {
+
+    '@es_extended/locale.lua',
+    'locales/fr.lua',
+    'server/esx_karting.lua',
+    'config.lua'
+}
+
+client_scripts {
+
+    '@es_extended/locale.lua',
+    'locales/fr.lua',
+    'config.lua',
+    'client/esx_karting.lua',
+}
