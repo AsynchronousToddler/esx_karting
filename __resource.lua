eda090ff8b2a760964a8cc257b2c5765219d7f9d
Guiyomee <guiyomee29200@gmail.com>
2018-03-18 11:15:56 +0100

    thank's xAN-S for translate in swedish

--- __resource.lua
+++ __resource.lua
@@ -5,6 +5,7 @@ server_scripts {
     '@es_extended/locale.lua',
     'locales/fr.lua',
     'locales/en.lua',
+    'locales/sv.lua',
     'server/esx_karting.lua',
     'config.lua'
 }
@@ -14,6 +15,7 @@ client_scripts {
     '@es_extended/locale.lua',
     'locales/fr.lua',
     'locales/en.lua',
+    'locales/sv.lua',
     'config.lua',
     'client/esx_karting.lua',
 }
