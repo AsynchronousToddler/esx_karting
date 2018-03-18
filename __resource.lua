24e5e7ee5bc0076d3d5d7d2ceb3dfd81e4cf36e5
Guiyomee <guiyomee29200@gmail.com>
2018-03-18 11:06:31 +0100

    add locales/en.lua

--- __resource.lua
+++ __resource.lua
@@ -4,6 +4,7 @@ server_scripts {
 
     '@es_extended/locale.lua',
     'locales/fr.lua',
+    'locales/en.lua',
     'server/esx_karting.lua',
     'config.lua'
 }
@@ -12,6 +13,7 @@ client_scripts {
 
     '@es_extended/locale.lua',
     'locales/fr.lua',
+    'locales/en.lua',
     'config.lua',
     'client/esx_karting.lua',
 }
